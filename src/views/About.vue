b4ba859d065d1af348714c2621fffc2d374b6837
Atlan <shiyang.hua@isee.work>
2021-07-31 13:17:09 +0800

    设置数字滚动组件

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,74 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul class="flex">
+      <scroll-num
+        v-for="(item, index) of numArr"
+        :key="index"
+        as="li"
+        :i="item"
+        :delay="index + 1"></scroll-num>
+    </ul>
+    <div class="logo-view">
+      <img class="image" src="./../assets/logo.png" alt="">
+    </div>
   </div>
 </template>
+
+<script>
+import ScrollNum from './../components/ScrollNumber.vue'
+export default {
+  components: {
+    ScrollNum
+  },
+  data() {
+    return {
+      num: 886
+    }
+  },
+  computed: {
+    numArr() {
+      const str = String(this.num)
+      let arr = []
+
+      for (let i = 0; i < str.length; i++) {
+        arr.push(parseInt(str[i]))
+      }
+
+      return arr
+    }
+  },
+  mounted() {
+    setInterval(() => this.num ++, 5000)
+  }
+}
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+ul {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
+.logo-view {
+  background: #ccc;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+.logo-view .image {
+  width: 100%;
+  height: 100%;
+}
+.logo-view .image:hover {
+  transform: rotate(666turn);
+  transition-delay: 1s;
+  transition-property: all;
+  transition-duration: 59s;
+  transition-timing-function: cubic-bezier(0.34, 0, 0.84, 1);
+}
+</style>
